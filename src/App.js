5ac0b875ec212df6be6092c078eb9dd30ff8f687
Ankush Meshram <ankushmeshram2403@gmail.com>
2023-03-21 13:29:45 +0530

    React redux

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ function App() {
   const dispatch = useDispatch();
 
   return (
-    <div className="App h-[100vh] flex justify-center">
-      <div className="my-[10%] w-80 h-[40vh] flex flex-col items-center justify-center bg-[#807f7f] shadow-md shadow-gray-500 rounded-md">
+    <div className="App h-[100vh] flex flex-col justify-center items-center">
 
-        <h3 className="text-5xl font-bold font-[Poppins]">{curState}</h3>
+      <h3 className="text-4xl font-bold text-gray-600">
+        Increment/Decrement counter
+      </h3>
+      <p className="text-lg text-gray-700 mt-4 font-bold">
+        using React and Redux
+      </p>
 
-        <div className="btns w-56 flex justify-between">
+      <div className="mt-6 btns w-32 h-10 flex justify-evenly items-center bg-gray-700 rounded-sm drop-shadow-md">
 
-          <button className="bg-[#3e82c1] w-40 h-8 mx-2 text-sm rounded-md border-none outline-none my-4 drop-shadow-md"
-          onClick={() => dispatch(Inc(5))}
-          >increment</button>
-
-          <button className="bg-[#3e82c1] w-40 h-8 mx-2 text-sm rounded-md border-none outline-none my-4 drop-shadow-md"
+        <button className="flex items-center justify-center text-2xl font-bold text-slate-900 text-center  cursor-pointer w-1/3 hover:text-red-300"
           onClick={() => dispatch(Dec(3))}
-          >decrement</button>
-        </div>
+          >-</button>
 
+        <h3 className="bg-gray-400 h-9 w-1/3 text-center items-center flex justify-center ">{curState}</h3>
+
+        <button className="flex items-center justify-center text-2xl font-bold text-slate-900 text-center  cursor-pointer w-1/3 h-full hover:text-red-300"
+          onClick={() => dispatch(Inc(3))}
+          >+</button>
       </div>
+
     </div>
   );
 }
